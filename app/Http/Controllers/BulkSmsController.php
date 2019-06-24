50b56c685bc0380bc3136bdb5a1d59c7ad33d505
Singh Hardil <hardilsingh87@gmail.com>
2019-06-24 23:56:30 +0530

    crd

--- app/Http/Controllers/BulkSmsController.php
+++ app/Http/Controllers/BulkSmsController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+
 use App\BulkSms;
 use App\Enrollment;
 use App\Http\Requests\BulkSmsRequest;
@@ -21,15 +21,6 @@ class BulkSmsController extends Controller
         return view('admin.bulk_sms.index', compact('messages'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -48,50 +39,5 @@ class BulkSmsController extends Controller
         $request->session()->flash('message_sent', 'Messages sent successfully');
 
         return redirect()->back();
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
